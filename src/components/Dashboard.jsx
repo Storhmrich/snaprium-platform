bef0759d8e7a457fd9ccf24d2ec5bc08fd28230d
storhmrich <storhmrichhh@gmail.com>
2026-06-13 09:25:15 +0000

    update codes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -173,14 +173,12 @@ export default function Dashboard({ isOpen, onClose, toggleTheme, theme }) {
     viewBox="0 0 24 24" 
     fill="none" 
     stroke="currentColor" 
-    strokeWidth="2.5" 
+    strokeWidth="2.75" 
     strokeLinecap="round" 
     strokeLinejoin="round"
   >
     <path d="M12 19V5" />
     <path d="M5 12L12 5L19 12" />
-    <path d="M19 19L18 18" />
-    <path d="M5 19L6 18" />
   </svg>
   Upgrade Plan
 </button>
